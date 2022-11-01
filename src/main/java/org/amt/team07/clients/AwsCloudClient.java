1425bfeeae856b110c11fbc83a2486223576a02a
Lazar <dev@lazarpavicevic.ch>
2022-11-01 13:27:07 +0100

    feat: add singleton pattern in aws client

--- src/main/java/org/amt/team07/clients/AwsCloudClient.java
+++ src/main/java/org/amt/team07/clients/AwsCloudClient.java
@@ -1,11 +1,24 @@
 package org.amt.team07.clients;
 
+import org.amt.team07.helpers.dataObjects.AwsDataObjectHelperImpl;
+import org.amt.team07.helpers.labelDetectors.AwsLabelDetectorHelperImpl;
+
 public class AwsCloudClient {
+    private static AwsCloudClient instance;
+
+    private AwsDataObjectHelperImpl dataObjectHelper;
+
+    private AwsLabelDetectorHelperImpl labelDetectorHelper;
+
     private AwsCloudClient() {
+        dataObjectHelper = new AwsDataObjectHelperImpl();
+        labelDetectorHelper = new AwsLabelDetectorHelperImpl();
     }
 
     public static AwsCloudClient getInstance() {
-        return null;
+        if (instance == null) {
+            instance = new AwsCloudClient();
+        }
+        return instance;
     }
-    
 }
