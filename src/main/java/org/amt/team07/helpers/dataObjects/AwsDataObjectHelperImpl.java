7c124ced8761df4ab0499fd5572b6c3bad3488bf
Jonathan <chil.tfm@gmail.com>
2022-11-01 15:34:59 +0100

    test: add test that were given in c#

--- src/main/java/org/amt/team07/helpers/dataObjects/AwsDataObjectHelperImpl.java
+++ src/main/java/org/amt/team07/helpers/dataObjects/AwsDataObjectHelperImpl.java
@@ -2,6 +2,25 @@ package org.amt.team07.helpers.dataObjects;
 
 public class AwsDataObjectHelperImpl {
 
+    public AwsDataObjectHelperImpl(String bucketUrl) {
+    }
+
+    public AwsDataObjectHelperImpl() {
+    }
+
     public void Create(String objectname) {
+    }
+
+    public void Create(String objectname, String path) {
+    }
+
+    public boolean Exists(String objectname) {
+        return false;
+    }
+
+    public void DownloadObject(String objectUrl, String destinationFullPath) {
+    }
+
+    public void RemoveObject(String bucketUrl) {
     }
 }
---
+++ src/test/java/TestAwsDataObjectHelperImpl.java
@@ -0,0 +1,189 @@
+import org.amt.team07.helpers.dataObjects.AwsDataObjectHelperImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestAwsDataObjectHelperImpl {
+
+    private AwsDataObjectHelperImpl bucketManager;
+    private String domain;
+    private String bucketName;
+    private String bucketUrl;
+    private String imageName;
+    private String pathToTestFolder;
+    private String fullPathToImage;
+    private String prefixObjectDownloaded;
+
+    @BeforeEach
+    public void Init()
+    {
+        this.pathToTestFolder = System.getProperty("user.dir").replace("bin\\Debug\\netcoreapp3.1", "testData");
+        this.bucketName = "testbucket";
+        this.domain = "aws.dev.actualit.info";
+        this.bucketUrl = bucketName + "." + this.domain;
+        this.imageName = "emiratesa380.jpg";
+        this.fullPathToImage = pathToTestFolder + "\\" + imageName;
+        this.prefixObjectDownloaded = "downloaded";
+        this.bucketManager = new AwsDataObjectHelperImpl(this.bucketUrl);
+    }
+
+    @Test
+     void CreateObject_CreateNewBucket_Success()
+    {
+        assertFalse(this.bucketManager.Exists(bucketUrl));
+
+        this.bucketManager.Create(bucketUrl);
+        
+        assertTrue(this.bucketManager.Exists(bucketUrl));
+    }
+
+    @Test
+    void CreateObject_CreateObjectWithExistingBucket_Success()
+    {
+        //given
+        String fileName = this.imageName;
+        String objectUrl = this.bucketUrl + "/" + this.imageName;
+        this.bucketManager.Create(this.bucketUrl);
+        
+        assertTrue(this.bucketManager.Exists(this.bucketUrl));
+        assertFalse(this.bucketManager.Exists(objectUrl));
+
+        //when
+        this.bucketManager.Create(objectUrl, this.pathToTestFolder + "//" + fileName);
+
+        //then
+        assertTrue(this.bucketManager.Exists(objectUrl));
+    }
+
+    @Test
+    void CreateObject_CreateObjectBucketNotExist_Success()
+    {
+        //given
+        String fileName = this.imageName;
+        String objectUrl = this.bucketUrl + "/" + this.imageName;
+        assertFalse(this.bucketManager.Exists(this.bucketUrl));
+        assertFalse(this.bucketManager.Exists(objectUrl));
+
+        //when
+        this.bucketManager.Create(objectUrl, this.pathToTestFolder + "//" + fileName);
+
+        //then
+        assertTrue(this.bucketManager.Exists(objectUrl));
+    }
+
+    @Test
+    void DownloadObject_NominalCase_Success()
+    {
+        //given
+        String objectUrl = bucketUrl + "//" + this.imageName;
+        String destinationFullPath = this.pathToTestFolder + "//" + this.prefixObjectDownloaded + this.imageName;
+        this.bucketManager.Create(objectUrl, this.pathToTestFolder + "//" + this.imageName);
+
+        assertTrue(this.bucketManager.Exists(bucketUrl));
+
+        //when
+        this.bucketManager.DownloadObject(objectUrl, destinationFullPath);
+
+        //then
+        File file = new File(destinationFullPath);
+        assertTrue(file.exists());
+    }
+
+    @Test
+    void Exists_NominalCase_Success()
+    {
+        //given
+        this.bucketManager.Create(this.bucketUrl);
+        boolean actualResult;
+
+        //when
+        actualResult = this.bucketManager.Exists(bucketUrl);
+
+        //then
+        assertTrue(actualResult);
+    }
+
+    @Test
+    void Exists_ObjectNotExistBucket_Success()
+    {
+        //given
+        String notExistingBucket = "notExistingBucket" + this.domain;
+        boolean actualResult;
+
+        //when
+        actualResult = this.bucketManager.Exists(notExistingBucket);
+
+        //then
+        assertFalse(actualResult);
+    }
+
+    @Test
+    void Exists_ObjectNotExistFile_Success()
+    {
+        //given
+        this.bucketManager.Create(this.bucketUrl);
+        String notExistingFile = bucketUrl + "//" + "notExistingFile.jpg";
+        assertTrue(this.bucketManager.Exists(bucketUrl));
+        boolean actualResult;
+
+        //when
+        actualResult = this.bucketManager.Exists(notExistingFile);
+
+        //then
+        assertFalse(actualResult);
+    }
+
+    @Test
+    void RemoveObject_EmptyBucket_Success()
+    {
+        //given
+        this.bucketManager.Create(this.bucketUrl);
+        assertTrue(this.bucketManager.Exists(bucketUrl));
+
+        //when
+        this.bucketManager.RemoveObject(this.bucketUrl);
+
+        //then
+        assertFalse(this.bucketManager.Exists(bucketUrl));
+    }
+
+    @Test
+    void RemoveObject_NotEmptyBucket_Success()
+    {
+        //given
+        String fileName = this.imageName;
+        String objectUrl = this.bucketUrl + "/" + this.imageName;
+        this.bucketManager.Create(this.bucketUrl);
+        this.bucketManager.Create(objectUrl, this.pathToTestFolder + "//" + fileName);
+
+        assertTrue(this.bucketManager.Exists(bucketUrl));
+        assertTrue(this.bucketManager.Exists(objectUrl));
+
+        //when
+        this.bucketManager.RemoveObject(this.bucketUrl);
+
+        //then
+        assertFalse(this.bucketManager.Exists(bucketUrl));
+    }
+
+    @AfterEach
+    void Cleanup(){
+        String destinationFullPath = this.pathToTestFolder + "\\" + this.prefixObjectDownloaded + this.imageName;
+
+        File file = new File(destinationFullPath);
+        if (file.exists())
+        {
+            file.delete();
+        }
+
+        this.bucketManager = new AwsDataObjectHelperImpl(this.bucketUrl);
+        if (this.bucketManager.Exists(bucketUrl))
+        {
+            this.bucketManager.RemoveObject(this.bucketUrl);
+        }
+    }
+ }
